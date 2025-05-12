0ad4a474250a0e320042ba687595f4d04c6f59c8
Renato Dinhani <renatodinhani@gmail.com>
2025-05-12 19:28:48 -0300

    menu

---
+++ dict.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class ProxyDict:
+    def __init__(self):
+        self.data = {}
+        self.count = defaultdict(int)
+
+    def __getitem__(self, key):
+        self.count[key] += 1
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def get(self, key, default=None):
+        if key in self.data:
+            self.count[key] += 1
+            return self.data[key]
+        return default
+
+    def untouched_keys(self):
+        return [k for k in self.data if self.count[k] == 0]
